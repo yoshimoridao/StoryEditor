52b3b028733fc638e239685cb44304704a21c8b5
trinh.daolenhat <trinh.daolenhat@gameloft.com>
2019-11-25 17:08:25 +0700

    + Add Label, Panel Objects

---
+++ trunk/Assets/Scripts/AddBtn.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddBtn : MonoBehaviour
+{
+    public enum FuncType { AddLabel, AddPanel };
+    public FuncType funcType = FuncType.AddLabel;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ trunk/Assets/Scripts/Label/LabelMgr.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LabelMgr : MonoBehaviour
+{
+    public ContentSizeFitter contentSize;
+    public Vector2 offset = new Vector2(5, 5);    // pixel
+
+    // prop
+    RectTransform rt;
+    [SerializeField]
+    Vector2 prevSizeDelta = Vector2.zero;
+
+    void Awake()
+    {
+        rt = GetComponent<RectTransform>();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+    }
+
+    public void OnEditDone()
+    {
+        // if (rt.sizeDelta != prevSizeDelta)
+        {
+            contentSize.enabled = false;
+            rt.sizeDelta = new Vector2(rt.sizeDelta.x + offset.x, rt.sizeDelta.y + offset.y);
+            // prevSizeDelta = rt.sizeDelta;
+            Debug.Log("resize done");
+        }
+    }
+
+    public void OnChangeValue()
+    {
+        if (!contentSize)
+            return;
+            
+        contentSize.enabled = false;
+        contentSize.enabled = true;
+        // prevSizeDelta = rt.sizeDelta;
+
+        Debug.Log("resize");
+    }
+}
